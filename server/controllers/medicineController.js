adfa3fe5912cf96f2991b58c4f9616afd55831fa
vignesh210561 <vigneshbandalkunta@gmail.com>
2025-09-08 22:52:34 +0530

    added the expiredMedicines,viewAllMedicines,deleteMedicine,searchMedicine

--- server/controllers/medicineController.js
+++ server/controllers/medicineController.js
@@ -1,3 +1,4 @@
+import medicineBatchModel from "../models/medicineBatchModel.js";
 import medicineModel from "../models/medicineModel.js";
 import rackModel from "../models/rackModel.js";
 
@@ -66,12 +67,78 @@ const addMedicine = async (req, res) => {
   }
 };
 
-const ExpiredMedicines = (req, res) => {
+const ExpiredMedicines = async (req, res) => {
+    try {
+        const expiredBatches = await medicineBatchModel
+        .find({ expiryDate: { $lt: new Date() } })
+        .populate("medicineId", "medicineCode")
+        .lean();
+
+         const expiredTablets = expiredBatches.map(batch => {
+            if (batch.medicineId) {
+                return {
+                batchNumber: batch.batchNumber,
+                medicineCode: batch.medicineId.medicineCode
+                };
+            }
+            return null;
+        })
+        .filter(Boolean);
+      
+        return res.json({success : true, expiredTablets });
+      
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({ success : false,error: error.message });
+    }
+  };
   
+
+const viewAllMedicines = async (req, res) => {
+
+    try{
+        const medicines = await medicineModel.find().lean();
+
+        const medicinesArray = [...medicines];
+
+        return res.json({ success : true, medicines: medicinesArray });
+    } catch(error) {
+
+        console.error(err);
+        return res.status(500).json({ success : false, error: error.message});
+    }
 };
 
-const viewAllMedicines = (req, res) => {
-  
-};
+const deleteMedicine = async (req,res) => {
+    const { medicineCode } = req.body;
 
-export { addMedicine, viewAllMedicines };
+    const deletedMedicine = await medicineModel.findOneAndDelete({ medicineCode });
+    
+    if (!deletedMedicine) {
+      return res.status(404).json({ success: false,error: "Medicine not found" });
+    }
+    
+    return res.json({success: true , message: "Medicine deleted successfully" });  
+}
+
+const searchMedicine = async (req,res) => {
+    try {
+        const { medicineCode } = req.body;
+    
+        if (!medicineCode) {
+          return res.status(400).json({ success: false, error: "medicineCode is required" });
+        }
+    
+        const medicine = await medicineModel.findOne({ medicineCode });
+    
+        if (!medicine) {
+          return res.status(404).json({ success: false, error: "Medicine not found" });
+        }
+    
+        return res.json({ success: true, medicine });
+    }catch (err) {
+        console.error(err);
+        return res.status(500).json({ success: false, error: "Server error" });
+    }
+}
+export { addMedicine, viewAllMedicines, ExpiredMedicines,deleteMedicine };
